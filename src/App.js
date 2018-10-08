c6489f8e4dfdeaff48092cf409aead5f94b7f357
Ovidio R. Calvet <calveto@gmail.com>
2018-10-08 13:31:03 -0400

    Add drawer from material examples

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './components/Header';
 class App extends Component {
   render() {
     return (
-      <div>
+      <div style={{ height: '100vh' }}>
         <Header />
       </div>
     );
