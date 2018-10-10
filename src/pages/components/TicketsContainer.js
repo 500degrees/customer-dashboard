f2f419f9f69d028cfb273c39d732c182e3e60554
Ovidio R. Calvet <calveto@gmail.com>
2018-10-10 00:39:49 -0400

    Add message when no tickets are available

--- src/pages/components/TicketsContainer.js
+++ src/pages/components/TicketsContainer.js
@@ -4,9 +4,9 @@ import TicketCard from './TicketCard';
 const TicketsContainer = ({ tickets, cleanAccount, transferAccount, closeTicket }) => {
   return (
     <div>
-      {tickets.map(t => (
+      {tickets && tickets.length > 0 ? tickets.map(t => (
         <TicketCard key={t._id} ticket={t} cleanAccount={cleanAccount(t._id)} transferAccount={transferAccount(t._id)} closeTicket={closeTicket(t._id)} />
-      ))}
+      )) : <div>No tickets... :)</div>}
     </div>
   )
 }
