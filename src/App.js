940f05b75750f21ed51c0cb791fe1720880b9a54
Ovidio R. Calvet <calveto@gmail.com>
2018-10-11 11:45:11 -0400

    Add sigin form

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import React, { Component } from 'react';
 import Layout from './components/Layout';
 import TicketsPage from './pages/TicketsPage';
+import LoginPage from './pages/LoginPage';
 
 export class App extends Component {
   state = {
-    loading: false
+    loading: false,
+    signedIn: false,
   }
 
   onLoadingUpdate = (isLoading) => {
     console.log('Updaing loading', isLoading)
     this.setState({ loading: isLoading });
   }
+  siginIn = () => {
+    this.setState({ signedIn: true });
+  }
 
   render() {
+    const page = this.state.signedIn ? 
+    <TicketsPage onLoadingUpdate={this.onLoadingUpdate} /> :
+    <LoginPage />
     return (
       <div style={{ height: '100vh' }}>
-        <Layout page={<TicketsPage onLoadingUpdate={this.onLoadingUpdate} />} loading={this.state.loading} />
+        <Layout page={page} loading={this.state.loading} />
       </div>
     );
   }
---
+++ src/pages/LoginPage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Signin from './components/Signin';
+
+export default () => {
+  return (
+    <Signin />
+  )
+}
